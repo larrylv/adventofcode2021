71e8c16c38b0665c1b6ac486226845038130e0ff
Larry Lv <larrylv1990@gmail.com>
2021-12-22 23:08:13 -0800

    day 23 -- solved by hand

---
+++ day_23.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'pqueue'
+require 'set'
+
+MAX_NUMBER = 1 << 64
+
+# lines = File.readlines(ARGV[0]).map(&:strip)
+
+# part one
+
+# 15299
+# solved by hand
+
+# part two
+
+# 47193
+# solved by hand
